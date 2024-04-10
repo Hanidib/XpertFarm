a14c72c81d3dee7d2fe2ecf148aa2f7cd83cfcca
Hani dib <hanidib21@gmail.com.com>
2024-04-10 15:43:41 +0300

    Authenticate And access token

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -4,12 +4,49 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 
 class UserController extends Controller
 {
     public function login(Request $request)
     {
-        dd($request->all());
+        $email = $request->get('email');
+        $password = $request->get('password');
+
+        if (Auth::attempt(compact('email', 'password'))) {
+            $user = auth()->user();
+            $access_token = $user->createToken('authtoken')->plainTextToken;
+            return response()->json([
+                'status ' => true,
+                'message' => "User Authenticated Successfully",
+                'token' => $access_token
+
+            ]);
+        } else {
+            return response()->json([
+                'status' => false,
+                'message' => 'Invalid Username Or Password '
+            ]);
+        }
+    }
+    public function register(Request $request)
+    {
+        $user = new User();
+        $user->name = $request->get('name');
+        $user->phone = $request->get('phone');
+        $user->email = $request->get('email');
+        $user->password = bcrypt($request->get('password'));
+        $user->save();
+
+        $access_token = $user->createToken('authtoken')->plainTextToken;
+
+        return response()->json([
+            'status ' => true,
+            'message' => "User Registered Successfully",
+            'token' => $access_token
+
+        ]);
     }
 }
