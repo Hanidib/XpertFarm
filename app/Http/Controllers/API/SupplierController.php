ba9ba21cb843b93aa7326d8c905b99ce97553b09
Hani dib <hanidib21@gmail.com.com>
2024-04-11 22:38:45 +0300

    Implement CRUD API's for Suppliers Create (Store),Read,Update,Delete

---
+++ app/Http/Controllers/API/SupplierController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SupplierRequest;
+use App\Models\Supplier;
+use Illuminate\Http\Request;
+
+class SupplierController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $suppliers = Supplier::all();
+        return response()->json([
+            'status' => true,
+            'data' => $suppliers,
+            'message' => 'List of Suppliers'
+        ]);
+    }
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(SupplierRequest $request)
+    {
+        $suppliers = Supplier::create($request->all());
+        return response()->json([
+            'status' => true,
+            'data' => $suppliers,
+            'message' => 'Supplier Created Successfully'
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $suppliers = Supplier::find($id);
+        if ($suppliers) {
+            return response()->json([
+                'status' => true,
+                'data' => $suppliers,
+                'message' => 'Suppliers Information'
+            ]);
+        } else {
+            return response()->json([
+                'status' => false,
+                'data' => null,
+                'message' => 'Supplier not found'
+            ]);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(SupplierRequest $request, string $id)
+    {
+        $suppliers = Supplier::find($id);
+        $suppliers = $suppliers->update($request->all());
+        return response()->json([
+            'status' => true,
+            'data' => $suppliers,
+            'message' => 'Suppliers Update Successfuly'
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $result = Supplier::destroy($id);
+        if ($result) {
+            return response()->json([
+                'status' => true,
+                'data' => null,
+                'message' => 'Suppliers Deleted Successfully'
+            ]);
+        } else {
+            return response()->json([
+                'status' => false,
+                'data' => null,
+                'message' => 'Suppliers not found'
+            ]);
+        }
+    }
+}
